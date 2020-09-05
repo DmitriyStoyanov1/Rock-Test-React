f48d69b957e115d85afa5ba18149ce113775e6be
danilstoyanov <danya.bright@yandex.ru>
2020-09-05 12:16:09 +0300

    Add build folder to the repo

---
+++ build/precache-manifest.ff9a9028b568fdc6b66685c164d2b889.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ded7f9600804808953b1ac9e4f2f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c89167c3f96dfdee59e7",
+    "url": "/static/css/main.5a6244b1.chunk.css"
+  },
+  {
+    "revision": "68044b715c2e00bb0651",
+    "url": "/static/js/2.9ad6f3ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9ad6f3ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89167c3f96dfdee59e7",
+    "url": "/static/js/main.2d2986aa.chunk.js"
+  },
+  {
+    "revision": "f4875aad38fcc20f3090",
+    "url": "/static/js/runtime-main.c60b2725.js"
+  },
+  {
+    "revision": "9c105cf45f71447728bca61116201687",
+    "url": "/static/media/batman-second-back.9c105cf4.jpg"
+  },
+  {
+    "revision": "e8f57325d2904486eec3437cc5df803e",
+    "url": "/static/media/rockhand.e8f57325.png"
+  },
+  {
+    "revision": "71fe9bff208e3c8605916e3440be0fb7",
+    "url": "/static/media/skulls.71fe9bff.jpg"
+  },
+  {
+    "revision": "bd61753db66d234f126ca37ec5a8a452",
+    "url": "/static/media/start.bd61753d.jpeg"
+  }
+]);
